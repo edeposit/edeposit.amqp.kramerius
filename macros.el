c4f27db75e0e68b9e152fe4b1233f114d2ae468b
Jan Stavěl <stavel.jan@gmail.com>
2015-07-29 19:02:02 +0200

    jstavel/edeposit#468 doplnil jsem sphinx dokumentaci.

--- macros.el
+++ macros.el
@@ -1,9 +1,17 @@
 (project-shell "*shell*" "/home/jan/work/edeposit.amqp.kramerius")
 (project-shell "*cmd*" "/home/jan/work/edeposit.amqp.kramerius")
 (project-shell "*tests*" "/home/jan/work/edeposit.amqp.kramerius")
+(project-shell "*docs*" "/home/jan/work/edeposit.amqp.kramerius/docs")
+(project-shell "*docs-serve*" "/home/jan/work/edeposit.amqp.kramerius/docs")
+
 
 (project-task send-message "*cmd*" "./send-message.sh")
 (project-task run-amqp "*shell*" "lein run -- --amqp")
+(project-task run-docs-make "*docs*" "make html")
+(project-task run-docs-serve "*docs-serve*" "python -m SimpleHTTPServer 8000")
+(project-task run-nosier-makedocs "*docs*" "nosier -b 'plans' -b '_build' -b '_static' -b '_download' 'make html'")
+
+(project-task run-readdocs "*cmd*" "google-chrome http://localhost:8000/_build/html/index.html&")
 
 (defun restart-app ()
   (interactive)
