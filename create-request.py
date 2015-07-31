32cd181ba32e5f62b7cb6d65812ca3b6d6eae746
Jan Stavěl <stavel.jan@gmail.com>
2015-07-31 17:49:59 +0200

    jstavel/edeposit#454 do struktury pro request jsem doplnil nahled prvni strany. Vytvoril jsem testovaci request.

--- create-request.py
+++ create-request.py
@@ -2,21 +2,24 @@
 import json
 import base64
 
-def createRequest(uuid, marcxml, fname, outname):
-    data = dict(filename=fname,
-                uuid=uuid,
-                __nt_name="ExportToKramerius",
-                b64_data="",
-                b64_marcxml=""
-    )
-    out = base64.encodestring(open(fname,'rb').read())
-    data['b64_data'] = out
-
-    out_marcxml = base64.encodestring(marcxml)
-    data['b64_marcxml'] = out_marcxml
+def createRequest(uuid, urnnbn, marcxml, full_fname, preview_fname, outname):
+    data = dict( __nt_name="ExportToKramerius",
+                 uuid=uuid,
+                 urnnbn=urnnbn,
+                 b64_marcxml=base64.encodestring(marcxml),
+                 img_full = dict(filename=full_fname.split('/')[-1],
+                                 mimetype='application/pdf',
+                                 b64_data = base64.encodestring(open(full_fname,'rb').read())),
+                 img_preview = dict(filename=preview_fname.split('/')[-1],
+                                    mimetype='image/jpeg',
+                                    b64_data = base64.encodestring(open(preview_fname,'rb').read()))
+             )
     open(outname,'wb').write(json.dumps(data))
     
 uuid='e65d9072-2c9b-11e5-99fd-b8763f0a3d61'
-createRequest(uuid, open('resources/oai_marc.xml','rb').read(),
-              'resources/robotandbaby.pdf', 'resources/export-request.json')
-
+urnnbn='urn:nbn:cz:mzk-0005ol'
+createRequest(uuid, urnnbn,
+              open('resources/oai_marc.xml','rb').read(),
+              'resources/robotandbaby.pdf', 
+              'resources/robotandbaby_001.jpg',
+              'resources/export-request.json')
