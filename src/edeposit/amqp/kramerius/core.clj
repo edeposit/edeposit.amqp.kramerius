459c30a1b5a65dc133edc072036983cb441127b3
Jan Stavěl <stavel.jan@gmail.com>
2015-10-05 18:49:15 +0200

    priprava na zaverecne lepeni vsech akci dohromady. edeposit/edeposit#635

--- src/edeposit/amqp/kramerius/core.clj
+++ src/edeposit/amqp/kramerius/core.clj
@@ -5,21 +5,20 @@
    [edeposit.amqp.kramerius.systems :refer [prod-system]]
    [reloaded.repl :refer [system init start stop go reset]]
    [edeposit.amqp.kramerius.handlers :as h]
-   [reagi.core :as r]
    )
   (:gen-class :main true)
 )
 
-(defn obs->obs
+(defn inputs->outputs
   "Takes input observables and transform them into output observables.
   This is main logic of kramerius.
 
-  It returns 3 Observables
+  It returns 3 channes
   - requests to convert marcxml2mods
   - requests to export result to ssh
   - response messages to send back a results
 
-  3 input Obs:
+  3 input channels:
   - requests to export
   - marcxml-to-mods responses
   - ssh export responses
@@ -32,21 +31,23 @@
   ## SSH export response
   "
   [request-obs marcxml-to-mods-response-obs ssh-response-obs]
-  (let  [ workdir-obs (->> request-obs
+  (comment
+    (let  [ workdir-obs (->> request-obs
                            (r/map h/request-with-tmpdir)
                            (r/map h/save-request))
          requests-to-marcxml2mods-obs (->> workdir-obs
                                            (r/map h/prepare-marcxml2mods-request))
          mods-files-obs (->> marcxml-to-mods-response-obs
-                                  (r/map h/save-marcxml2mods-response)
-                                  (r/map h/parse-mods-files)
-                                  )
+                             (r/map h/save-marcxml2mods-response)
+                             (r/map h/parse-mods-files)
+                             )
          requests-to-ssh-obs (->> mods-files-obs
                                   (r/map h/mods->oai_dcs)
                                   (r/map h/make-package-with-foxml mods-files-obs) ;; zip two observables mods, oai_dcs
                                   )]
-    { :requests-to-marcxml2mods-obs requests-to-marcxml2mods-obs
-     :requests-to-ssh requests-to-ssh-obs})
+      { :requests-to-marcxml2mods-obs requests-to-marcxml2mods-obs
+       :requests-to-ssh requests-to-ssh-obs})
+    )
   )
 
 (defn -main [& args]
