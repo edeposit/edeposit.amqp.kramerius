9f54b9e2b1f1c8fd9e2aff2ef54f9bb48c3a57d1
Jan Stavěl <stavel.jan@gmail.com>
2015-12-23 13:13:23 +0100

    added system for production environment

--- src/edeposit/amqp/kramerius/core.clj
+++ src/edeposit/amqp/kramerius/core.clj
@@ -9,72 +9,9 @@
   (:gen-class :main true)
 )
 
-;; (defn ->marcxml2mods
-;;   [requests]
-;;   (let [saved (->> requests
-;;                    (z/map h/request-with-tmpdir)
-;;                    (z/map h/save-request))]
-;;     {:saved saved
-;;      :marcxml2mods-requests (->> saved (z/map h/prepare-marcxml2mods-request))
-;;      :preview-pages (->> saved (z/map h/make-preview-page))}
-;;     )
-;;   )
-
-;; (defn ->storage
-;;   [marcxml2mods-responses preview-pages]
-;;   (let [mods (->> marcxml2mods-responses
-;;                   (z/map h/save-marcxml2mods-response)
-;;                   (z/map h/parse-mods-files)
-;;                   (z/map h/add-urnnbn-to-mods))
-;;         oai_dcs (->> mods (z/map h/mods->oai_dcs))]
-;;     {:mods mods :oai_dcs oai_dcs})
-;;   )
-
-;; (defn inputs->outputs
-;;   "Takes input observables and transform them into output observables.
-;;   This is main logic of kramerius.
-
-;;   It returns 3 channes
-;;   - requests to convert marcxml2mods
-;;   - requests to export result to ssh
-;;   - response messages to send back a results
-
-;;   3 input channels:
-;;   - requests to export
-;;   - marcxml-to-mods responses
-;;   - ssh export responses
-
-;;   ## Request to export
-;;   [metadata payload]
-
-;;   ## Marcxml-to-mods response
-  
-;;   ## SSH export response
-;;   "
-;;   [request-obs marcxml-to-mods-response-obs ssh-response-obs]
-;;   (comment
-;;     (let  [ workdir-obs (->> request-obs
-;;                              (r/map h/request-with-tmpdir)
-;;                              (r/map h/save-request))
-;;            requests-to-marcxml2mods-obs (->> workdir-obs
-;;                                              (r/map h/prepare-marcxml2mods-request))
-;;            mods-files-obs (->> marcxml-to-mods-response-obs
-;;                                (r/map h/save-marcxml2mods-response)
-;;                                (r/map h/parse-mods-files)
-;;                                )
-;;            requests-to-ssh-obs (->> mods-files-obs
-;;                                     (r/map h/mods->oai_dcs)
-;;                                     (r/map h/make-package-with-foxml mods-files-obs) ;; zip two observables mods, oai_dcs
-;;                                     )]
-;;       { :requests-to-marcxml2mods-obs requests-to-marcxml2mods-obs
-;;        :requests-to-ssh requests-to-ssh-obs})
-;;     )
-;;   )
-
 (defn -main [& args]
     (let [ [options args banner] 
          (cli/cli args
-                  [ "-f" "--file"]
                   [ "--amqp" :default false :flag true]
                   [ "-h" "--help" :default false :flag true]
                   )
@@ -87,9 +24,6 @@
       (defonce server (start-server :port 12345))
       (reloaded.repl/set-init! prod-system)
       (go)
-      )
-    (when (:file options)
-      (println "file")
       )
     )
   )
--- src/edeposit/amqp/kramerius/systems.clj
+++ src/edeposit/amqp/kramerius/systems.clj
@@ -1,19 +1,177 @@
 (ns edeposit.amqp.kramerius.systems
   (:require [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
+            [edeposit.amqp.kramerius.components :as c]
+            [edeposit.amqp.kramerius.handlers :as h]
             [edeposit.amqp.kramerius.components :refer [amqp-middleware
                                                         rabbit-mq]]))
 
 (defn prod-system []
   (component/system-map
-   :kramerius-amqp (rabbit-mq (env :kramerius-amqp-uri))
-   :marcxml2mods-amqp (rabbit-mq (env :marcxml2mods-amqp-uri))
-   :storage-amqp (rabbit-mq (env :storage-amqp-uri))
-
-   :kramerius-middleware-marcxml2mods (->
-                                       (amqp-middleware 
-                                        )
-                                       (component/using :kramerius-amqp)
-                          )
+   :kramerius (rabbit-mq (env :kramerius-amqp-uri))
+   :marcxml2mods (rabbit-mq (env :marcxml2mods-amqp-uri))
+   :storage (rabbit-mq (env :storage-amqp-uri))
+   :middleware-marcxml2mods (-> (amqp-middleware
+                                 {:exchanges [:convert-to-mods]
+                                  :queues [
+                                           [:kramerius
+                                            :routing-keys [[:convert-to-mods :response]]
+                                            :handler (-> h/save-marcxml2mods-response
+                                                         c/raw-pass
+                                                         c/to-clojure
+                                                         (c/send-result-to :kramerius :internal
+                                                                           :with-key :marcxml2mods.response)
+                                                         c/ack)
+                                            ]
+                                           ]
+                                  }
+                                 )
+                                (component/using { :rabbit :marcxml2mods
+                                                   :kramerius :kramerius })
+                                )
+   :middleware-storage (-> (amqp-middleware
+                            {:exchanges [:export]
+                             :queues [
+                                      [:kramerius
+                                       :routing-keys [[:export :response]]
+                                       :handler (-> h/save-response-from-export-to-storage
+                                                    c/raw-pass
+                                                    c/to-clojure
+                                                    (c/send-result-to :kramerius :internal
+                                                                      :with-key :storage.response)
+                                                    c/ack)
+                                       ]
+                                      ]
+                             }                            
+                            )
+                           (component/using {:rabbit :storage
+                                             :kramerius :kramerius})
+                           )
+   :middleware-kramerius (-> (amqp-middleware
+                              {:exchanges [:export-to-kramerius :internal]
+                                :queues [[:request-saver
+                                          :routing-keys [[:export-to-kramerius :request]]
+                                          :handler (-> (comp h/save-request
+                                                             h/request-with-tmpdir)
+                                                       c/raw-pass
+                                                       c/to-clojure
+                                                       (c/send-result-to :kramerius :internal
+                                                                         :with-key :request-save.response)
+                                                       c/ack)
+                                          ]
+                                         [:preview-page-maker
+                                          :routing-keys [[:internal :request-save.response]]
+                                          :handler (-> h/make-preview-page
+                                                       c/from-clojure
+                                                       c/to-clojure
+                                                       (c/send-result-to :kramerius :internal
+                                                                         :with-key :make-preview.response)
+                                                       c/ack)
+                                          ]
+                                         [:marcxml2mods
+                                          :routing-keys [[:internal :request-save.response]]
+                                          :handler (-> h/prepare-marcxml2mods-request
+                                                       c/from-clojure
+                                                       c/to-json
+                                                       (c/send-result-to :marcxml2mods
+                                                                         :convert-to-mods
+                                                                         :with-key :request)
+                                                       c/ack)
+                                          ]
+                                         [:mods
+                                          :routing-keys [[:internal :marcxml2mods.response]]
+                                          :handler (-> (comp h/add-urnnbn-to-mods
+                                                             h/parse-mods-files)
+                                                       c/from-clojure
+                                                       c/to-clojure
+                                                       (c/send-result-to :kramerius :internal
+                                                                         :with-key :mods.created)
+                                                       c/ack)
+                                          ]
+                                         [:foxml
+                                          :routing-keys [[:internal :mods.created]]
+                                          :handler (-> (comp h/make-zip-package
+                                                             (partial apply h/make-package-with-foxml)
+                                                             h/with-oai_dcs)
+                                                       c/from-clojure
+                                                       c/to-clojure
+                                                       (c/send-result-to :kramerius :internal
+                                                                         :with-key :foxml-package.created)
+                                                       c/ack)
+                                          ]
+                                         [:storage
+                                          :routing-keys [[:internal :foxml-package.created]]
+                                          :handler  (-> h/prepare-request-for-export-to-storage
+                                                        c/from-clojure
+                                                        c/to-json
+                                                        (c/send-result-to :storage :export
+                                                                          :with-key :request)
+                                                        c/ack)
+                                          
+                                          
+                                          ]
+                                         [:email
+                                          :routing-keys [[:internal :storage.response]]
+                                          :handler (->
+                                                    (comp
+                                                     (h/sendmail pc/send-message)
+                                                     ;; (h/sendmail (fn [msg]
+                                                     ;;               {:code 0,
+                                                     ;;                :error :SUCCESS,
+                                                     ;;                :message "message sent"}))
+                                                     h/save-email-at-workdir
+                                                     (h/make-email :from (env :email-to-kramerius-from)
+                                                                   :to (env :email-to-kramerius-from))
+                                                     (fn [x] (h/prepare-email
+                                                             x
+                                                             :import-mount (env :import-mount)
+                                                             :archive-mount (env :archive-mount)
+                                                             :originals-mount (env :originals-mount)
+                                                             )
+                                                       )
+                                                     )
+                                                    c/from-clojure
+                                                    c/to-clojure
+                                                    (c/send-result-to :kramerius :internal
+                                                                      :with-key :email-to-kramerius.sent)
+                                                    c/ack)
+                                          ]
+                                         [:scp
+                                          :routing-keys [[:internal :storage.response]]
+                                          :handler (->
+                                                    (comp
+                                                     (fn [x]
+                                                       (h/scp-to-kramerius x {:scp (fn [from-path to-path]
+                                                                                     (str from-path
+                                                                                          "->"
+                                                                                          to-path))})
+                                                       )
+                                                     (fn [x] (h/prepare-scp-to-kramerius
+                                                             x
+                                                             :import-mount (env :import-mount)
+                                                             :archive-mount (env :archive-mount)
+                                                             :originals-mount (env :originals-mount)
+                                                             )
+                                                       )
+                                                     )
+                                                    c/from-clojure
+                                                    c/to-clojure
+                                                    (c/send-result-to :kramerius :internal
+                                                                      :with-key :scp-package.sent)
+                                                    c/ack)
+                                          ]
+                                         [:rest
+                                          :routing-keys [[:internal :scp-package.sent]]
+                                          :handler (fn [component ch metadata payload]
+                                                     (println "scp-package sent" metadata payload)
+                                                     )
+                                          ]
+                                         ]
+                               }
+                              )
+                             (component/using {:rabbit :kramerius})
+                             (component/using [:kramerius :marcxml2mods :storage])
+                             )
    )
-)
+  )
+
